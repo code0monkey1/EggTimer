0ff768e663efb7bc5e5af68bbadec7bedb5fa948
Chiranjeev Thomas <chiranjeevcollege@gmail.com>
2018-10-16 15:49:53 +0530

    Added Gif

--- app/src/main/java/com/example/chiranjeev/eggtimer/MainActivity.java
+++ app/src/main/java/com/example/chiranjeev/eggtimer/MainActivity.java
@@ -1,21 +1,28 @@
 package com.example.chiranjeev.eggtimer;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Animatable;
 import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import static com.example.chiranjeev.eggtimer.R.drawable.egg;
 
 public class MainActivity extends AppCompatActivity {
     private Button start;
     private SeekBar seek;
     private TextView time;
     private MediaPlayer mediaPlayer;
-    private long currentTime;
+    private ImageView eggPicture;
 
+    private long currentTime;
+    private boolean buttonPressed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,20 +64,31 @@ public class MainActivity extends AppCompatActivity {
         seek.setProgress(1);
     }
 
+
     private void init() {
         start = findViewById(R.id.start);
         mediaPlayer = MediaPlayer.create(this, R.raw.alert);
         seek = findViewById(R.id.seek);
         time = findViewById(R.id.time);
+        eggPicture = findViewById(R.id.eggImage);
     }
 
     public void onStartClick(View view) {
 
-        time = findViewById(R.id.time);
+        if (buttonPressed == false) {
+            time = findViewById(R.id.time);
 
-        final long countDownTime = getProcessedTime();
+            final long countDownTime = getProcessedTime();
 
-        startCountdownTimer(countDownTime);
+            startCountdownTimer(countDownTime);
+
+            buttonPressed = true;
+
+            start.setText("Stop");
+        } else {
+            start.setText("Start");
+            buttonPressed = false;
+        }
     }
 
     private long getProcessedTime() {
@@ -82,7 +100,17 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTick(long millisUntilFinished) {
+
                 displayTime(millisUntilFinished);
+                eggPicture.animate().rotationBy(360);
+                terminateIfStopButtonPressed();
+            }
+
+            private void terminateIfStopButtonPressed() {
+                if (buttonPressed == false) {
+                    seek.setEnabled(true);
+                    cancel();
+                }
             }
 
             @Override
@@ -98,11 +126,11 @@ public class MainActivity extends AppCompatActivity {
         mediaPlayer.start();
 
         enableControls(true);
+
     }
 
     private void enableControls(boolean b) {
         seek.setEnabled(b);
-        start.setEnabled(b);
     }
 
     private void displayTime(long millisUntilFinished) {
